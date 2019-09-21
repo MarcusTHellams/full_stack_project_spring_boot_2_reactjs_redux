09279295cc163544c916854010de83bc58e200ed
Marcus T. Hellams <mhellams@hotmail.com>
2019-09-21 18:27:52 -0400

    Custom Exception

--- PPMToolFullStack/src/main/java/io/mthdigital/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/io/mthdigital/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.mthdigital.ppmtool.services;
 
 import io.mthdigital.ppmtool.domain.Project;
+import io.mthdigital.ppmtool.exceptions.ProjectIdException;
 import io.mthdigital.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,12 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }
+        catch(Exception e){
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
     }
 }
