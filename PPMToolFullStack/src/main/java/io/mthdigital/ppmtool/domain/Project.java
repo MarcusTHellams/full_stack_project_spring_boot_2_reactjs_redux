8753b73c77b7e9a82d03cdd03f2ecfdd3645a9be
Marcus T. Hellams <mhellams@hotmail.com>
2019-09-21 19:35:49 -0400

    Up to 13 Find all Projects

--- PPMToolFullStack/src/main/java/io/mthdigital/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/io/mthdigital/ppmtool/domain/Project.java
@@ -21,13 +21,13 @@ public class Project {
     @NotBlank(message = "Project description is required")
     private String description;
 
-    @JsonFormat(pattern = "yyyy-mm-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date start_date;
-    @JsonFormat(pattern = "yyyy-mm-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date end_date;
-    @JsonFormat(pattern = "yyyy-mm-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date created_at;
-    @JsonFormat(pattern = "yyyy-mm-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date updated_at;
 
     public Project() {
